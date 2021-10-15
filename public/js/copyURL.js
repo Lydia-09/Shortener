e72d6883ff64956f1ce0758467b59fe303913cbb
Lydia-09 <pasky0901@gmail.com>
2021-10-15 15:56:55 +0800

    feat: add copy short URL function

---
+++ public/js/copyURL.js
@@ -0,0 +1,10 @@
+function copyURL() {
+  const shortURL = document.getElementById("shortURL")
+  const selection = window.getSelection()
+  const range = document.createRange()
+  range.selectNodeContents(shortURL)
+  selection.removeAllRanges()
+  selection.addRange(range)
+  document.execCommand('copy')
+  alert("已複製短網址連結: " + shortURL)
+}
